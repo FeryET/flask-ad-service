72c6f91c03a757f77b9a704c69a5b66d580a4e14
Farhood Etaati <farhoodetaati@gmail.com>
2023-11-22 12:46:34 +0330

    chore: creates global conftest for tests
    adds fixtures for flask and database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,29 @@
 # Copyright 2019 by J. Christopher Wagner (jwag). All rights reserved.
-from typing import Any
-from unittest.mock import MagicMock
+import unittest.mock
+from collections.abc import Generator
+from typing import Any, Literal
 
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient, FlaskCliRunner
-from pytest_mock import MockerFixture
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import Session
+
+from flask_ad_service.config import generate_app_config
+from flask_ad_service.database import db
 
 
-def generate_test_config() -> dict[Any, Any]:
-    from flask_ad_service.config import generate_app_config
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
+
+@pytest.fixture(scope="session")
+def _generate_testing_config():
     config = generate_app_config()
     config["TESTING"] = True
     config["WTF_CSRF_ENABLED"] = False
@@ -19,32 +32,77 @@ def generate_test_config() -> dict[Any, Any]:
     # Make this plaintext for most tests - reduces unit test time by 50%
     config["SECURITY_PASSWORD_HASH"] = "plaintext"
 
-    config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///memory"
+    config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
 
-    return config
+    with unittest.mock.patch(
+        "flask_ad_service.app.generate_app_config", return_value=config
+    ):
+        yield
 
 
-@pytest.fixture(scope="function")
-def app(mocker: MockerFixture) -> "Flask":
+@pytest.fixture(scope="session")
+def app(_generate_testing_config) -> Generator["Flask", Any, None]:
     """
     App for unittesting the package.
     """
 
     from flask_ad_service.app import create_app
 
-    config = generate_test_config()
-    mocker.patch(
-        "flask_ad_service.app.generate_app_config", new=MagicMock(return_value=config)
-    )
     app = create_app()
-    return app
+
+    yield app
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
+def app_ctx(app: Flask) -> Generator[None, Any, None]:
+    with app.app_context():
+        yield
+
+
+@pytest.fixture(scope="session")
+def database(app_ctx: None) -> Generator[SQLAlchemy, Any, None]:
+    db.create_all()
+    yield db
+    db.session.rollback()
+    db.drop_all()
+
+
+@pytest.fixture(scope="module")
+def db_test_session(
+    app_ctx: None,
+    database: SQLAlchemy,
+) -> Generator[Session, Any, None]:
+    database.session.rollback()
+    database.drop_all()
+    database.session.close_all()
+    database.create_all()
+    from tests.faker.factories import (
+        AdFactory,
+        CommentFactory,
+        RoleFactory,
+        UserFactory,
+    )
+
+    for item in [AdFactory, RoleFactory, UserFactory, CommentFactory]:
+        item._meta.reset_sequence()
+    yield database.session  # type: ignore
+
+
+@pytest.fixture(scope="session")
 def client(app: Flask) -> FlaskClient:
     return app.test_client()
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="module")
 def runner(app: Flask) -> FlaskCliRunner:
     return app.test_cli_runner()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def faker_session_locale() -> list[str]:
+    return ["en_US"]
+
+
+@pytest.fixture(scope="session", autouse=True)
+def faker_seed() -> Literal[12345]:
+    return 12345
