67b86534d7e59f7e226890effa28cc4e5505c6bc
Farhood Etaati <farhoodetaati@gmail.com>
2023-11-22 12:33:35 +0330

    chore: sets up flask security in the app factory

--- src/flask_ad_service/app.py
+++ src/flask_ad_service/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_security.core import Security
+from flask_security.datastore import SQLAlchemyUserDatastore
 
 from flask_ad_service.config import generate_app_config
 
@@ -8,6 +10,7 @@ def create_app() -> Flask:
     app = Flask(__name__)
     app.config.update(generate_app_config())
     setup_db(app)
+    setup_flask_security(flask_app=app)
     return app
 
 
@@ -20,6 +23,14 @@ def setup_db(flask_app: Flask) -> None:
     with flask_app.app_context():
         db.create_all()
 
+
+def setup_flask_security(flask_app: Flask) -> None:
+    """Set up the flask security extension."""
+    from flask_ad_service.database import db
+    from flask_ad_service.model import RoleDBModel, UserDBModel
+
+    user_datastore = SQLAlchemyUserDatastore(db, UserDBModel, RoleDBModel)  # type: ignore
+    flask_app.security = Security(flask_app, user_datastore)  # type: ignore
 
 
 if __name__ == "__main__":
